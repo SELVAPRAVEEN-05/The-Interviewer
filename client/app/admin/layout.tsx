96f2471c04f148e0e92aef0f78db06229fb4a9f0
SELVAPRAVEEN-05 <selvapraveen2005@gmail.com>
2025-07-31 23:09:47 +0530

    sidebar

--- client/app/admin/layout.tsx
+++ client/app/admin/layout.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
 export default function AdminLayout() {
-  return (
-    <div className='flex h-screen w-full'>
-        <div className='flex justify-between items-center bg-gray-800 text-white p-4 w-2/12 h-full'>
-sidebar
+    return (
+        <div className='flex h-screen w-full'>
+            <div className='bg-gray-800 text-white p-4 w-2/12 h-full'>
+                <p>CodeMeet</p>
+            </div>
+            <div>
+                AdminDashboard
+            </div>
         </div>
-        <div>
-AdminDashboard
-        </div>
-    </div>
-  )
+    )
 }
