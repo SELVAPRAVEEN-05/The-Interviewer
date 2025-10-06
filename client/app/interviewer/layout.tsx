e71e607e57c958597b6906fa6dbd0570a3582fb2
SELVAPRAVEEN-05 <selvapraveen2005@gmail.com>
2025-10-06 22:52:20 +0530

    started interviewer

--- client/app/interviewer/layout.tsx
+++ client/app/interviewer/layout.tsx
@@ -1,9 +1,45 @@
 "use client";
 
-export default function AdminLayout({
+import SideBar from "@/components/atoms/sidebar";
+import { BookAIcon, LayoutDashboard, Users } from "lucide-react";
+import Navbar from "./components/navBar";
+
+export default function CandidateLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <div className="h-[100dvh] w-screen overflow-hidden">{children}</div>;
+  const sidebarItems = [
+    {
+      id: "dashboard",
+      label: "Dashboard",
+      icon: <LayoutDashboard size={18} />,
+    },
+    {
+      id: "interviewListes",
+      label: "upcomming Interview",
+      icon: <Users size={18} />,
+    },
+    {
+      id: "shortlistcandidates",
+      label: "ShortList Candidate",
+      icon: <BookAIcon size={18} />,
+    }
+  ];
+
+  return (
+    <div className="h-[100dvh] w-screen overflow-hidden">
+      <div className="h-[10vh] w-full">
+        <Navbar />
+      </div>
+      <div className="flex h-[90vh] w-full overflow-hidden">
+        <div className="h-full">
+          <SideBar sidebarItems={sidebarItems} />
+        </div>
+        <div className="flex-1 p-6 overflow-y-auto scrollbar-hide overflow-hidden">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
 }
